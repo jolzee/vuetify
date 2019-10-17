1c75afef48066d68d93c59989171a64d9f0bfe01
Peter Joles <36912049+jolzee@users.noreply.github.com>
2019-10-17 15:11:59 -0700

    Added updated mdi icons
    This template was not rendering correctly because of icon naming issues - v2.15 of Vuetify

--- packages/docs/src/examples/layouts/google-contacts.vue
+++ packages/docs/src/examples/layouts/google-contacts.vue
@@ -31,7 +31,7 @@
             v-else-if="item.children"
             :key="item.text"
             v-model="item.model"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            :prepend-icon="item.model ? item.icon : item['mdi-icon-alt']"
             append-icon=""
           >
             <template v-slot:activator>
@@ -46,7 +46,6 @@
             <v-list-item
               v-for="(child, i) in item.children"
               :key="i"
-              @click=""
             >
               <v-list-item-action v-if="child.icon">
                 <v-icon>{{ child.icon }}</v-icon>
@@ -61,7 +60,6 @@
           <v-list-item
             v-else
             :key="item.text"
-            @click=""
           >
             <v-list-item-action>
               <v-icon>{{ item.icon }}</v-icon>
@@ -93,7 +91,7 @@
         flat
         solo-inverted
         hide-details
-        prepend-inner-icon="search"
+        prepend-inner-icon="mdi-file-document-box-search"
         label="Search"
         class="hidden-sm-and-down"
       ></v-text-field>
@@ -116,7 +114,8 @@
             src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
             alt="Vuetify"
           >
-          </v-img></v-avatar>
+          </v-img>
+        </v-avatar>
       </v-btn>
     </v-app-bar>
     <v-content>
@@ -180,52 +179,37 @@
         </v-card-title>
         <v-container>
           <v-row>
-            <v-col
-              class="align-center justify-space-between"
-              cols="12"
-            >
-              <v-row align="center">
-                <v-avatar
-                  size="40px"
-                  class="mr-4"
-                >
-                  <img
-                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                    alt=""
-                  >
-                </v-avatar>
-                <v-text-field
-                  placeholder="Name"
-                ></v-text-field>
-              </v-row>
+            <v-col cols="12">
+              <v-text-field
+                prepend-icon="mdi-mail"
+                placeholder="Name"
+              ></v-text-field>
             </v-col>
             <v-col cols="6">
               <v-text-field
-                prepend-icon="business"
+                prepend-icon="mdi-account"
                 placeholder="Company"
               ></v-text-field>
             </v-col>
             <v-col cols="6">
-              <v-text-field
-                placeholder="Job title"
-              ></v-text-field>
+              <v-text-field placeholder="Job title"></v-text-field>
             </v-col>
             <v-col cols="12">
               <v-text-field
-                prepend-icon="mail"
+                prepend-icon="mdi-mail"
                 placeholder="Email"
               ></v-text-field>
             </v-col>
             <v-col cols="12">
               <v-text-field
                 type="tel"
-                prepend-icon="phone"
+                prepend-icon="mdi-phone"
                 placeholder="(000) 000 - 0000"
               ></v-text-field>
             </v-col>
             <v-col cols="12">
               <v-text-field
-                prepend-icon="notes"
+                prepend-icon="mdi-card-text"
                 placeholder="Notes"
               ></v-text-field>
             </v-col>
@@ -253,45 +237,43 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
-    },
-    data: () => ({
-      dialog: false,
-      drawer: null,
-      items: [
-        { icon: 'contacts', text: 'Contacts' },
-        { icon: 'history', text: 'Frequently contacted' },
-        { icon: 'content_copy', text: 'Duplicates' },
-        {
-          icon: 'keyboard_arrow_up',
-          'icon-alt': 'keyboard_arrow_down',
-          text: 'Labels',
-          model: true,
-          children: [
-            { icon: 'add', text: 'Create label' },
-          ],
-        },
-        {
-          icon: 'keyboard_arrow_up',
-          'icon-alt': 'keyboard_arrow_down',
-          text: 'More',
-          model: false,
-          children: [
-            { text: 'Import' },
-            { text: 'Export' },
-            { text: 'Print' },
-            { text: 'Undo changes' },
-            { text: 'Other contacts' },
-          ],
-        },
-        { icon: 'settings', text: 'Settings' },
-        { icon: 'chat_bubble', text: 'Send feedback' },
-        { icon: 'help', text: 'Help' },
-        { icon: 'phonelink', text: 'App downloads' },
-        { icon: 'keyboard', text: 'Go to the old version' },
-      ],
-    }),
-  }
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      { icon: "mdi-contacts", text: "Contacts" },
+      { icon: "mdi-history", text: "Frequently contacted" },
+      { icon: "mdi-content-duplicate", text: "Duplicates" },
+      {
+        icon: "mdi-chevron-up",
+        "mdi-icon-alt": "mdi-chevron-down",
+        text: "Labels",
+        model: true,
+        children: [{ icon: "mdi-add", text: "Create label" }]
+      },
+      {
+        icon: "mdi-chevron-up",
+        "mdi-icon-alt": "mdi-chevron-down",
+        text: "More",
+        model: false,
+        children: [
+          { text: "Import" },
+          { text: "Export" },
+          { text: "Print" },
+          { text: "Undo changes" },
+          { text: "Other contacts" }
+        ]
+      },
+      { icon: "mdi-settings", text: "Settings" },
+      { icon: "mdi-comment-quote-outline", text: "Send feedback" },
+      { icon: "mdi-help", text: "Help" },
+      { icon: "mdi-download-multiple", text: "App downloads" },
+      { icon: "mdi-keyboard", text: "Go to the old version" }
+    ]
+  })
+};
 </script>
